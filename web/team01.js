db5f884b9e2a4f267e0ef13415b133deace94aae
Josh Spendlove <joshvspendlove@gmail.com>
2021-01-11 18:24:39 -0700

    updated team01.js

--- web/team01.js
+++ web/team01.js
@@ -1,13 +1,15 @@
+var resetTimer = null;
+
 function clickMe(id)
 {
-    console.log(id)
     var button = document.getElementById(id);
     button.innerHTML = "Thanks for Clicking";
-    setTimeout(resetButton(), 2500, id);
+    resetTimer = setInterval(resetButton(), 2500, id);
 }
 
 function resetButton(id)
 {
     var button = document.getElementById(id);
     button.innerHTML = "Click Me";
+    clearInterval(resetTimer);
 }
