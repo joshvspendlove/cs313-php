16297306dd09c2d37953c2747faa2fa5c9cf67c4
Josh Spendlove <joshvspendlove@gmail.com>
2021-01-16 13:04:31 -0700

    updated index.php to include more php code.

--- web/index.php
+++ web/index.php
@@ -21,6 +21,53 @@
 	  <div class="card">
 	    <div class="jumbotron">
 	      <h1>Welcome to my website!</h1>
+	          <?php $today = "Today is " . date("l");
+		  $month = date("m");
+		  $month_word = "";
+		  switch ($month) {
+		    case 1:
+		      $month_word = "January";
+		      break;
+		    case 2:
+		      $month_word = "February";
+		      break;
+		    case 3: 
+		      $month_word = "March";
+		      break;
+		    case 4:
+		      $month_word = "April";
+		      break;
+		    case 5:
+		      $month_word = "May";
+		      break;
+		    case 6: 
+		      $month_word = "June";
+		      break;
+		    case 7:
+		      $month_word = "July";
+		      break;
+		    case 8:
+		      $month_word = "February";
+		      break;
+		    case 9: 
+		      $month_word = "March";
+		      break;
+		    case 10:
+		      $month_word = "April";
+		      break;
+		    case 11:
+		      $month_word = "May";
+		      break;
+		    case 12: 
+		      $month_word = "June";
+		      break;
+		    default:
+		      $month_word = "Doomed";
+		  }
+		  
+		  $today = $today . ", " . $month_word . " " . date("d, Y") . ".";
+		  echo "<p>$today</p>";
+		  ?>
 	  	  <p>This page is currently under construction. Please view my "Intro" page by clicking the link above or by clicking <a href="../intro.php">here</a>.</p>
 		</div>
     </div>
