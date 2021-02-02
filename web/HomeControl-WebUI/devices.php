173aaf82b22a26d3d431750cfe7f31aa64eb3078
Josh Spendlove <joshvspendlove@gmail.com>
2021-02-02 14:24:45 -0700

    added Home

---
+++ web/HomeControl-WebUI/devices.php
@@ -0,0 +1,52 @@
+<?php require 'ConnectDB.php'; ?>
+<?php 
+	$devices = array();
+	foreach ($db->query("SELECT * FROM devices WHERE systemid = 1;") as $row)
+	{
+		$devices[$row['devicetype']]['dbDeviceID'] = $row['dbdeviceid'];
+		$devices[$row['devicetype']]['deviceid'] = $row['deviceid'];
+	}
+
+?>
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <?php require "./header.php"; ?>
+    <title>Home Control WebUI</title>
+  </head>
+  <body class="container-fluid">
+    <div class="menu">
+      <?php require 'webui-menu.php';
+    </div>
+    <div class="content">
+	  <div class="card-group">
+        <div class="card">
+          <div class="card-header">
+            <h1>Lights</h1>
+		  </div>
+		  <div class="card-body">
+			<?php
+				foreach($devices['light'] as $light)
+				{
+					foreach ($db->query("SELECT * FROM lights WHERE dbdeviceid = '$light['dbdeviceid']';") as $row)
+					{	
+						echo $row['lightname'] . $row['lightlevel'];
+					}
+	{
+				}
+			?>			
+		  </div>
+		</div>
+	  </div>
+      <br>
+      <div class="card">
+        <div class="card-header">
+          <h1>Locks</h1>
+        </div>
+        <div class="card-body">
+           
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ web/HomeControl-WebUI/webui-menu.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo '<nav class="navbar navbar-expand-sm bg-light navbar-light">';
+    echo '<div class="navbar-nav">';
+    echo '<a href="../assignments.php" class="menuLink">Home</a>';
+    echo '<a href="../devices.php" class="menuLink">Devices</a>';
+    echo '</div>';
+    echo '</nav>';
+
+?>
