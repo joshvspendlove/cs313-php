5a6bf9eab6825b8ee825158cc030be302419149a
Josh Spendlove <joshvspendlove@gmail.com>
2021-02-04 18:43:34 -0700

    made updates

--- web/HomeControl-WebUI/webui-menu.php
+++ web/HomeControl-WebUI/webui-menu.php
@@ -4,6 +4,9 @@
     echo '<div class="navbar-nav">';
     echo '<a href="../assignments.php" class="menuLink">Home</a>';
     echo '<a href="./devices.php" class="menuLink">Devices</a>';
+    echo '<form action="./login.php" method="get">';
+    echo '<label>Username</label><input type="text" name='username'/>';
+    echo '<label>Password</label><input type="text" name='password'/>';
     echo '</div>';
     echo '</nav>';
 
