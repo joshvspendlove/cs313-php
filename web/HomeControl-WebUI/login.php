9e085ff8bc32144daf6364f980c4a654e906dc65
Josh Spendlove <joshvspendlove@gmail.com>
2021-02-04 19:47:40 -0700

    made updates

--- web/HomeControl-WebUI/login.php
+++ web/HomeControl-WebUI/login.php
@@ -1,29 +1,27 @@
 <?php
 	include './ConnectDB.php';
 	include './SessionController.php';
+
+	unset($_SESSION['systemid']);
 	
 	if (isset($_GET['username']) and isset($_GET['password']))
 	{
 		$username = $_GET['username'];
 		$password = $_GET['password'];
-
-		$system = $db->query("SELECT systemid FROM users WHERE username = '$username' AND userpass = '$password';");
-		echo gettype($system);
-		if (!empty($system))
+	
+		foreach($db->query("SELECT systemid FROM users WHERE username = '$username' AND userpass = '$password';") as $system)
 		{
 			$_SESSION['systemid'] = $system['systemid'];
-		
 		}
-		else
+		if(!isset($_SESSION['systemid']))
 		{
 			$_SESSION['systemid'] = 0;
 		}
-		echo $_SESSION['systemid'];
-//		header("Location: ./devices.php");
+		
+		header("Location: ./devices.php");
 	}
 	else 
 	{
-		unset($_SESSION['systemid']);
 		header("Location: ./devices.php"); 
 	}
 	
