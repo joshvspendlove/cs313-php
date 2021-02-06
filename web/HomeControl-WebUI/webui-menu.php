dfc399cb70ebd0e400447ba5cf6472165f1ec857
Josh Spendlove <joshvspendlove@gmail.com>
2021-02-06 10:27:02 -0700

    made updates

--- web/HomeControl-WebUI/webui-menu.php
+++ web/HomeControl-WebUI/webui-menu.php
@@ -8,13 +8,13 @@
     echo '<div class="navbar-nav mr-auto">';
     echo '<a href="../assignments.php" class="nav-link">Home</a>';
     echo '<a href="./devices.php" class="nav-link">Devices</a>';
-	echo '<form action="./login.php" method="post">';
+	echo '<form class="form-inline my-2 my-lg-0" action="./login.php" method="post">';
 
     if($_SESSION['loggedin'] == False)
     {
-		echo '<label>Username</label><input type="text" name="username"/>';
-    	echo '<label>Password</label><input type="password" name="password"/>';
-    	echo '<input type="Submit" value="Login"/>';
+		echo '<label>Username</label><input class="form-control mr-sm-2"  type="text" name="username"/>';
+    	echo '<label>Password</label><input class="form-control mr-sm-2"  type="password" name="password"/>';
+    	echo '<input class="btn btn-outline-success my-2 my-sm-0" type="Submit" value="Login"/>';
     }
     else
     {
