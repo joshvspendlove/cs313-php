2f381d167cf3c63e2cbedde1d118d03aeaac91e6
Josh Spendlove <joshvspendlove@gmail.com>
2021-02-06 10:17:25 -0700

    made updates

--- web/HomeControl-WebUI/webui-menu.php
+++ web/HomeControl-WebUI/webui-menu.php
@@ -1,23 +1,27 @@
 <?php
 
     echo '<nav class="navbar navbar-expand-sm bg-light navbar-light">';
-    echo '<div class="navbar-nav d-flex">';
-    echo '<a class="mr-auto p-2" href="../assignments.php" class="menuLink">Home</a>';
-    echo '<a class="mr-auto p-2" href="./devices.php" class="menuLink">Devices</a>';
-    echo '<form class="p-2" action="./login.php" method="post">';
+    echo '<div class="navbar-nav">';
+    echo '<a href="../assignments.php" class="menuLink">Home</a>';
+    echo '<a href="./devices.php" class="menuLink">Devices</a>';
+    echo '<span class="navbar-text">';
+	echo '<form action="./login.php" method="post">';
 
     if($_SESSION['loggedin'] == False)
     {
-		echo '<label class="p-2">Username</label><input class="p-2" type="text" name="username"/>';
-    	echo '<label class="p-2">Password</label><input class="p-2" type="password" name="password"/>';
-    	echo '<input class="p-2" type="Submit" value="Login"/>';
+		echo '<label>Username</label><input type="text" name="username"/>';
+    	echo '<label>Password</label><input type="password" name="password"/>';
+    	echo '<input type="Submit" value="Login"/>';
     }
     else
     {
-	echo '<input class="p-2" type="Submit" value="Logout"/>';
+	echo '<input type="Submit" value="Logout"/>';
     }
     echo '</form>';
+	echo '</span>';
     echo '</div>';
     echo '</nav>';
 
 ?>
+
+
