32b949c681114695df321b09d35183603ae1d82b
Josh Spendlove <joshvspendlove@gmail.com>
2021-02-06 09:50:17 -0700

    made updates

--- web/HomeControl-WebUI/webui-menu.php
+++ web/HomeControl-WebUI/webui-menu.php
@@ -4,11 +4,11 @@
     echo '<div class="navbar-nav">';
     echo '<a href="../assignments.php" class="menuLink">Home</a>';
     echo '<a href="./devices.php" class="menuLink">Devices</a>';
-    echo '<form action="./login.php" method="post">';
+    echo '<form class="login_form" action="./login.php" method="post">';
 
     if($_SESSION['loggedin'] == False)
     {
-	echo '<label>Username</label><input type="text" name="username"/>';
+		echo '<label>Username</label><input type="text" name="username"/>';
     	echo '<label>Password</label><input type="password" name="password"/>';
     	echo '<input type="Submit" value="Login"/>';
     }
