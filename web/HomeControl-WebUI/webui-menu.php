b26da1c5008ed97d878bca54c3a2151043ae45e6
Josh Spendlove <joshvspendlove@gmail.com>
2021-02-06 10:24:45 -0700

    made updates

--- web/HomeControl-WebUI/webui-menu.php
+++ web/HomeControl-WebUI/webui-menu.php
@@ -1,35 +1,13 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar w/ text</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-    </ul>
-    <span class="navbar-text">
-      Navbar text with an inline element
-    </span>
-  </div>
-</nav>
+
 
 
 
 <?php
-/*
+
     echo '<nav class="navbar navbar-expand-sm bg-light navbar-light">';
-    echo '<div class="navbar-nav">';
-    echo '<a href="../assignments.php" class="menuLink">Home</a>';
-    echo '<a href="./devices.php" class="menuLink">Devices</a>';
-    echo '<span class="navbar-text">';
+    echo '<div class="navbar-nav mr-auto">';
+    echo '<a href="../assignments.php" class="nav-link">Home</a>';
+    echo '<a href="./devices.php" class="nav-link">Devices</a>';
 	echo '<form action="./login.php" method="post">';
 
     if($_SESSION['loggedin'] == False)
@@ -43,10 +21,9 @@
 	echo '<input type="Submit" value="Logout"/>';
     }
     echo '</form>';
-	echo '</span>';
     echo '</div>';
     echo '</nav>';
-*/
+
 ?>
 
 
