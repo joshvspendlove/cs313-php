b00d7b20db3fa126490392f2c03b36d2f9e8c849
Josh Spendlove <joshvspendlove@gmail.com>
2021-02-11 13:03:56 -0700

    updated handleRequests.php

--- web/HomeControl-WebUI/devicecontrol.js
+++ web/HomeControl-WebUI/devicecontrol.js
@@ -18,5 +18,6 @@ function toggleSwitch(id)
 		}
 	};
 	xhttp.open("POST", "handleRequests.php", true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhttp.send('DATA={"request":"update_device","device_data":{"' + id +'":"' + state + '"}');
 }
