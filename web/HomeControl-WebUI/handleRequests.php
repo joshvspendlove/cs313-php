6b6d57c84a727a28544eb3005f5cc895c8f42028
Josh Spendlove <joshvspendlove@gmail.com>
2021-02-11 08:46:03 -0700

    updated handleRequests.php

--- web/HomeControl-WebUI/handleRequests.php
+++ web/HomeControl-WebUI/handleRequests.php
@@ -3,5 +3,51 @@ require './ConnectDB.php';
 var_dump($_POST);
 echo 'request received';
 
+if (htmlspecialchars($_POST['DATA']) != null)
+{
+	$data = json_encodehtmlspecialchars($_POST['DATA']);
+	
+	
+	$request = $data['request'];
+
+	switch($request)
+	{
+		case 'add_device':
+			 $new_devices = htmlspecialchars($_POST['new_devices']);
+			 add_device();
+		     break;
+
+		case 'update_device':
+			 $device_data = htmlspecialchars($_POST['device_data']);
+			 update_device();
+		     break;
+
+		case 'get_device_state':
+			 get_device_state();
+		     break;
+
+	}
+}
+else
+{
+	echo 'Request Failed';
+}
+
+function add_device()
+{
+	$devices = json_encode($new_devices);
+	echo 'add_device()';
+}
+
+function update_device()
+{
+	$updated_data = json_encode($device_data);
+	echo 'update_device()';
+}
+
+function get_device_state()
+{
+	echo 'get_device_state()';
+}
 ?>
 
