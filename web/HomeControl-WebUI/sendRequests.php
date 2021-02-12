a62f50db60ce5bcdc2c8c59cfb682b1a4cf25bc5
Josh Spendlove <joshvspendlove@gmail.com>
2021-02-12 11:54:16 -0700

    updated handleRequests.php

--- web/HomeControl-WebUI/sendRequests.php
+++ web/HomeControl-WebUI/sendRequests.php
@@ -1,5 +1,5 @@
 <?php
-	include './handleRequests.php';
+	
 	
 	if (isset($_POST['DATA']))
 	{
@@ -18,9 +18,11 @@
 				}
 				//$device = json_decode($data, true);
 				$_POST['DATA'] = array('device_data' => array('device' => $data));
-				update_device($_POST['DATA']);
+				//update_device($_POST['DATA']);
 			}				
 		}
 	}
+	
+	include './handleRequests.php';
 
 ?>
