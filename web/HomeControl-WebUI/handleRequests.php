e6d7341e04d8b2e4d0c39f6aa0bbd046aed3fbc5
Josh Spendlove <joshvspendlove@gmail.com>
2021-02-12 12:01:46 -0700

    updated handleRequests.php

--- web/HomeControl-WebUI/handleRequests.php
+++ web/HomeControl-WebUI/handleRequests.php
@@ -44,8 +44,8 @@ function add_device()
 function update_device($device_data)
 {
 	echo 'update_device()';
-	var_dump($device_data);
-	foreach ($device_data['device'] as $device)
+
+	foreach ($device_data['device_data'] as $device)
 	{
 		echo $device['deviceid'] . '-' . $device['lightlevel'];
 	}
